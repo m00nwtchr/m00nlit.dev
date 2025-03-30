4e0a2809a4c1b23772a88d12d52d3c0f6952d648
m00nwtchr <m00nwtchr@duck.com>
2025-03-30 20:47:23 +0200

    Flake/direnv

--- flake.nix
+++ flake.nix
@@ -12,9 +12,7 @@
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+      pkgs = import nixpkgs {inherit system;};
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
